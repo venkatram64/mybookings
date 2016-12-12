c72c362a65ba121fd89fea318122a213796b9adc
venkatram64 <venkat.veerareddy@hotmail.com>
2016-12-12 07:14:22 +0530

    added functionality

--- src/main/java/com/venkat/booking/ViewController.java
+++ src/main/java/com/venkat/booking/ViewController.java
@@ -1,5 +1,9 @@
 package com.venkat.booking;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +17,22 @@ import java.util.Date;
 @Controller
 public class ViewController {
 
+    @Value("${app-mode}")
+    private String appMode;
+
+   /* private String getAppMode;
+
+    @Autowired
+    public ViewController(Environment environment){
+        appMode = environment.getProperty("app-mode");
+    }
+    */
+
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("datetime", new Date());
         model.addAttribute("username", "Srijan Veerareddy");
-        model.addAttribute("mode", "development");
+        model.addAttribute("mode", appMode);
         return "index";
     }
 }
